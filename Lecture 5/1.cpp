2461f19d2b08d3452c052bc7b053fc018971fef4
Sanyam Lawania <39163842+Sylar009@users.noreply.github.com>
2024-11-16 18:09:10 +0530

    First Cpp Code

---
+++ Lecture 5/1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+/*
+    Complier search for the 
+    main function to execute 
+    the C++ program.
+*/
+int main() {
+    // printing hello world
+    cout << "Hello World - by TechVerse Tutorials.";
+    return 0;
+}
