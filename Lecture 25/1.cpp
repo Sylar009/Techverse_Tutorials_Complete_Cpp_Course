5ca33d41fc8a3bd858d90d4f1784bcc3c0551b92
Sanyam Lawania <39163842+Sylar009@users.noreply.github.com>
2024-12-02 10:36:16 +0530

    Lecture 25

---
+++ Lecture 25/1.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+using namespace std; 
+
+int main() { 
+    // Initialize variables 
+    int num1 = 8; 
+    int num2 = 5; 
+
+    // Comparisons using relational operators
+    cout << "num1 > num2 = " << (num1 > num2) << endl;  // Greater than
+    cout << "num1 < num2 = " << (num1 < num2) << endl;  // Less than
+    cout << "num1 == num2 = " << (num1 == num2) << endl; // Equal to
+    cout << "num1 != num2 = " << (num1 != num2) << endl; // Not equal to
+
+    return 0; 
+}
