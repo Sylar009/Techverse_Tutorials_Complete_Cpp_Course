0920730b358e546f155966c426d8a318c475e575
Sanyam Lawania <39163842+Sylar009@users.noreply.github.com>
2024-12-28 12:56:36 +0530

    Lecture 34

---
+++ Lecture 34/1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cerr << "Error: Unable to connect to the server!" << endl;
+    cout << "Message: Connection attempt logged." << endl;
+
+    return 0;
+}
---
+++ Lecture 34/2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cerr << "Critical Error: Disk space full!" << endl;
+    cout << "Tip: Clear some files to free up space." << endl;
+
+    return 0;
+}
