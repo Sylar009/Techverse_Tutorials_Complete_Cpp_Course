fdc5dceb9a7925194f8ff10c130d622df5d13e53
Sanyam Lawania <39163842+Sylar009@users.noreply.github.com>
2025-03-14 14:19:40 +0530

    Lecture 50

---
+++ Lecture 50/2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void exchange(int& num1, int& num2) {
+    int temp = num1;
+    num1 = num2;
+    num2 = temp;
+}
+
+int main() {
+    int a = 8, b = 15;
+    cout << "Before Swap: a = " << a << ", b = " << b << endl;
+
+    exchange(a, b);
+
+    cout << "After Swap: a = " << a << ", b = " << b << endl;
+    return 0;
+}
