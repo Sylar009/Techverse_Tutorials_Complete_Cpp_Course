6f5c865d7a3befb5a9c92083b4ac039a7e16214f
Sanyam Lawania <39163842+Sylar009@users.noreply.github.com>
2025-03-15 15:39:53 +0530

    Lecture 65

---
+++ Lecture 65/2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void swapNumbers(int &num1, int &num2) {
+    int temp = num1;
+    num1 = num2;
+    num2 = temp;
+}
+
+int main() {
+    int first = 50, second = 30;
+
+    cout << "Before Swap:" << endl;
+    cout << "First: " << first << " Second: " << second << endl;
+
+    swapNumbers(first, second);
+
+    cout << "After Swap (Using Reference):" << endl;
+    cout << "First: " << first << " Second: " << second << endl;
+
+    return 0;
+}
